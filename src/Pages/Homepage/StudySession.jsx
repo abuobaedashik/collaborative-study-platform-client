dbaa4e1d48d76b7398be19a1b504663b4e2da09d
abuobaedashik <abuobaedashik@gmail.com>
2025-02-26 23:15:08 +0600

    session are building

--- src/Pages/Homepage/StudySession.jsx
+++ src/Pages/Homepage/StudySession.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/Auth/Authprovider";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { FaClock } from "react-icons/fa";
+import moment from "moment/moment";
 
 const StudySession = () => {
-  const [sessions, setsessions] = useState();
+  const [sessions, setsessions] = useState([]);
   const { user } = useContext(AuthContext);
   const axiosPublic = useAxiosPublic();
 
@@ -14,15 +15,18 @@ const StudySession = () => {
     });
   }, [axiosPublic]);
 
-//   useEffect(() => {
-//     const today = new Date();
-//     const courseStartDate = new Date(startDate);
-//     if (today > courseStartDate) {
-//       setStatus("Closed");
-//     } else {
-//       setStatus("Ongoing");
-//     }
-//   }, [startDate]);
+  const currentDate = moment().format("YYYY-MM-DD");
+
+
+  //   useEffect(() => {
+  //     const today = new Date();
+  //     const courseStartDate = new Date(startDate);
+  //     if (today > courseStartDate) {
+  //       setStatus("Closed");
+  //     } else {
+  //       setStatus("Ongoing");
+  //     }
+  //   }, [startDate]);
   console.log(sessions);
   return (
     <div>
@@ -31,35 +35,54 @@ const StudySession = () => {
         study session{" "}
       </div>
       <div className="mt-5 grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10 items-center justify-between mb-24 ">
-        {sessions?.map((session) => (
-          <div key={session._id} className="">
-            <div className=" bg-white shadow-lg rounded-2xl overflow-hidden">
-              <img
-                src={session?.banner}
-                alt={session?.title}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4 mx-6 min-h-[200px] flex items-center justify-center flex-col">
-                <h2 className="text-xl font-bold text-gray-800">{session.title}</h2>
-                <p className="text-gray-600 text-sm mt-2">{session.description.slice(0, 140)}....</p>
-                <div className="flex justify-between items-center gap-8 mt-4">
-                  <button className="py-2 font-extrabold rounded-lg bg-[#0A033C] text-[#ffffff] px-5 ">Read More</button>
-                  {/* <span
-                    className={`badge ${
-                      status === "Ongoing" ? "badge-success" : "badge-error"
-                    }`}
-                  >
-                    <FaClock className="inline-block mr-1" /> {status}
-                  </span> */}
-                  <button className="py-2 rounded-lg font-bold bg-[#0A033C] text-[#ffff00] px-5">Ongoing</button>
+        {sessions.map((session) => {
+          const isClosed = moment(session.endDate).isBefore(moment(), "day");
+
+          return (
+            <div key={session._id} className="">
+              <div className=" bg-white shadow-lg rounded-2xl overflow-hidden">
+                <img
+                  src={session?.banner}
+                  alt={session?.title}
+                  className="w-full h-48 object-cover"
+                />
+                <div className="p-4 mx-6 min-h-[200px] flex items-center justify-center flex-col">
+                  <h2 className="text-xl font-bold text-gray-800">
+                    {session.title}
+                  </h2>
+                  <p className="text-gray-600 text-sm mt-2">
+                    {session.description.slice(0, 140)}....
+                  </p>
+                  <div className="flex justify-between items-center gap-8 mt-4">
+                    <button className="py-2 font-extrabold rounded-lg bg-[#0a033cec] text-[#ffffff] px-5 ">
+                      Read More
+                    </button>
+                    <button
+                      className={` px-4 py-2 rounded-lg font-bold   ${
+                        isClosed
+                          ? "bg-[#F3CFCF] text-red-600"
+                          : "bg-[#f3cfcfb1] text-green-500"
+                      }`}
+                    >
+                      {isClosed ? "Closed" : "Ongoing"}
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
 };
 
 export default StudySession;
+
+// {sessions.map((session) => {
+//   const isClosed = moment(session.endDate).isBefore(moment(), "day");
+
+//   return (
+
+//   );
+// })}
